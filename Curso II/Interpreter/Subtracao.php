01c9bce80822045d68ca66884b6eb3fac159562d
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-07-30 17:57:38 -0300

    Criando a interface e classes que serão alteradas pelo padrão Interpreter.

---
+++ Curso II/Interpreter/Subtracao.php
@@ -0,0 +1,13 @@
+<?php
+
+class Subtracao implements Expressao
+{
+    private $esquerda;
+    private $direita;
+
+    public function __construct(Expressao $esquerda, Expressao $direita)
+    {
+        $this->esquerda = $esquerda;
+        $this->direita = $direita;
+    }
+}
