096b174b356dfda0825b6f8cd9966d9d38514b7b
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-07-30 15:39:24 -0300

    Criando classe Histórico que guarda uma lista de Contratos

---
+++ Curso II/Historico.php
@@ -0,0 +1,20 @@
+<?php
+
+class Historico
+{
+    private $estadosSalvos;
+
+    public function __construct()
+    {
+        $this->$estadosSalvos =  array();
+    }
+
+    public function pega($index)
+    {
+        return $this->estadosSalvos[index];
+    }
+    public function adiciona(Estado $estado)
+    {
+        $this->estadosSalvos[] = $estado;
+    }
+}
