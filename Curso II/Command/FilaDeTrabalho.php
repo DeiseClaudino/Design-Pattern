c603ea75cbb5dff0deb0199508777f67099ffb4b
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-08-01 16:45:02 -0300

    Criando classes que são fases do processo de pedido e criando métodos na FilaDeTrabalho.

--- Curso II/Command/FilaDeTrabalho.php
+++ Curso II/Command/FilaDeTrabalho.php
@@ -1,5 +1,23 @@
 <?php
 
-class FilaDeTrabalho implements Comando
+class FilaDeTrabalho
 {
+    private $comandos;
+
+    public function __construct()
+    {
+        $this->comandos = array();
+    }
+
+    public function adiciona(Comando $comando)
+    {
+        $this->comandos[] = ($comando);
+    }
+
+    public function processa()
+    {
+        foreach ($comandos as $comando) {
+            $comando->executa();
+        }
+    }
 }
